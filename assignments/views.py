25d96f6588fd2e89522f7406daaebfc120997405
jegorami <amidzic.branko@gmail.com>
2017-05-12 22:26:15 +0200

    Separate submit answer and report viewsets
     Necessary for generation of appropriate api documentation.
     In order to successfully inspect serializer fields, schema uses serializer class value,
     so every endpoint has to define own serializer class

--- assignments/views.py
+++ assignments/views.py
@@ -1,13 +1,20 @@
-from django.shortcuts import get_object_or_404
-from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.viewsets import ReadOnlyModelViewSet, ViewSet
+from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from assignments.models import Assignment
 from assignments.serializers import AssignmentSerializer, ReportAssignmentSerializer, SubmitAnswersSerializer
 
 
 class AssignmentViewSet(ReadOnlyModelViewSet):
+    """
+    retrieve:
+    Return the given assignment.
+
+    list:
+    Return a list of all the existing assignments.
+    """
+
     serializer_class = AssignmentSerializer
     lookup_field = 'slug'
 
@@ -18,16 +25,70 @@ class AssignmentViewSet(ReadOnlyModelViewSet):
         return Assignment.objects.filter(status=Assignment.STATUS_OPEN)
 
 
-class SubmitAnswersViewSet(ViewSet):
+class SubmitAnswersViewSet(CreateModelMixin, GenericViewSet):
+    """
+    Submit assignment answers
+
+    - **Purpose**: Submit answers for specified assignment.
+    - **Example of input json**
+        ```
+        {
+            "school": 1,
+            "school_class": 1,
+            "open_text_tasks": [
+            {
+                "task": 2,
+                "answer": "My first answer"
+            }],
+            "budgeting_targets": [{
+                "task": 1,
+                "target": 1,
+                "amount": 200,
+                "point": [123.343, 444.1232]
+                }
+            ],
+            "voluntary_tasks": [{
+                "first_name": "",
+                "last_name": "last",
+                "email": "test@test.com",
+                "phone": "43423",
+                "description": "school/class",
+                "lat": 60.45148,
+                "long": 22.26869
+            }]
+        }
+        ```
+    """
+    serializer_class = SubmitAnswersSerializer
 
     def get_serializer_context(self):
-        context = {
-            'assignment_slug': self.kwargs['slug']
-        }
+        context = super(SubmitAnswersViewSet, self).get_serializer_context()
+        if 'slug' in self.kwargs:
+            # if not api documentation
+            context['assignment_slug'] = self.kwargs['slug']
         return context
 
-    def get_report_context(self):
-        context = self.get_serializer_context()
+
+class ReportAssignmentViewSet(RetrieveModelMixin, GenericViewSet):
+    """
+    Get answers
+
+    - **Purpose**: Get all answers for specified assignment on report generation.
+    Answers can be additionally filtered by school and school class using GET query
+    params
+    - **GET query params**:
+        - *school*: school id
+        - *school_class*: school class id
+    - **Example usage**:
+        `https://www.example.com/api/report/<slug>/?school=1&&school_class=2`
+    """
+    serializer_class = ReportAssignmentSerializer
+
+    queryset = Assignment.objects.all()
+    lookup_field = 'slug'
+
+    def get_serializer_context(self):
+        context = super(ReportAssignmentViewSet, self).get_serializer_context()
         task_query_params = {
             'school': self.request.query_params.get('school', None),
             'school_class': self.request.query_params.get('school_class', None)
@@ -36,14 +97,3 @@ class SubmitAnswersViewSet(ViewSet):
             'query_params': task_query_params
         })
         return context
-
-    def create(self, request, *args, **kwargs):
-        answer = SubmitAnswersSerializer(data=request.data, context=self.get_serializer_context())
-        answer.is_valid(raise_exception=True)
-        answer.save()
-        return Response(status=status.HTTP_201_CREATED)
-
-    def list(self, request, *args, **kwargs):
-        assignment = get_object_or_404(Assignment, slug=kwargs['slug'])
-        serializer = ReportAssignmentSerializer(assignment, context=self.get_report_context())
-        return Response(serializer.data)
